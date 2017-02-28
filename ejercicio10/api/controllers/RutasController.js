8bb66e323bef8c5c1226267b5a6d714328882128
afre <a.frex_b@hotmail.com>
2017-02-28 15:12:39 -0500

    Parte4Rutas

--- ejercicio10/api/controllers/RutasController.js
+++ ejercicio10/api/controllers/RutasController.js
@@ -6,65 +6,120 @@
  */
 
 module.exports = {
-home:function(req,res){
-    return res.view('homepage',{
-        title:'Inicio'
-    });
-},
-    crearUsuario:function(req,res){
-        return res.view('Usuario/crearUsuario',{
-            title:'Crear Usuarios'
+    home: function (req, res) {
+        return res.view('homepage', {
+            title: 'Inicio'
+        });
+    },
+    crearUsuario: function (req, res) {
+        return res.view('Usuario/crearUsuario', {
+            title: 'Crear Usuarios'
         })
     },
-    listarUsuario:function(req,res){
-        Usuario.find().exec(function(error,usuariosEncontrados){
-            if(error)return res.view('error',{
-                title:'Error',
-                error:{
-                    descripcion:'Error al listar usuario: '+error,
-                    url:'/listarUsuario'
+    listarUsuario: function (req, res) {
+        Usuario.find().exec(function (error, usuariosEncontrados) {
+            if (error) return res.view('error', {
+                title: 'Error',
+                error: {
+                    descripcion: 'Error al listar usuario: ' + error,
+                    url: '/Usuario/listarUsuario'
                 }
             });
-        return res.view('Usuario/listarUsuario',{
-            title:'Lista de Usuarios',
-            usuarios:usuariosEncontrados
+            return res.view('Usuario/listarUsuario', {
+                title: 'Lista de Usuarios',
+                usuarios: usuariosEncontrados
+            });
         });
-        });
-        
+
     },
-    editarUsuario:function(req,res){
-        var parametros=req.allParams();
-        if(parametros.id){
+    editarUsuario: function (req, res) {
+        var parametros = req.allParams();
+        if (parametros.id) {
             Usuario.findOne({
-                id:parametros.id
-            }).exec(function(error,usuarioEncontrado){
-                if(error)return res.view('error',{
-                title:'Error',
-                error:{
-                    descripcion:'Error al editar usuario: '+error,
-                    url:'/editarUsuario'
-                }
-            });
-                return res.view('Usuario/editarUsuario',{
-                    title: 'Editar Usuario -'+usuarioEncontrado.nombre,
+                id: parametros.id
+            }).exec(function (error, usuarioEncontrado) {
+                if (error) return res.view('error', {
+                    title: 'Error',
+                    error: {
+                        descripcion: 'Error al editar usuario: ' + error,
+                        url: '/Usuario/editarUsuario'
+                    }
+                });
+                return res.view('Usuario/editarUsuario', {
+                    title: 'Editar Usuario -' + usuarioEncontrado.nombre,
                     usuarios: usuarioEncontrado
                 })
             })
-        }else{
-            res.view('error',{
-                title:'Error',
-                error:{
-                    descripcion:'Error con el ID',
-                    url:'/editarUsuario'
+        } else {
+            res.view('error', {
+                title: 'Error',
+                error: {
+                    descripcion: 'Error con el ID',
+                    url: '/Usuario/editarUsuario'
                 }
             });
         }
     },
-    
-    crearAgarre:function(req,res){},
-    listarAgarre:function(req,res){},
-    editarAgarre:function(req,res){},
-    
-    
-};
 
+    crearAgarre: function (req, res) {
+        return res.view('Usuario/crearUsuario', {
+            title: 'Crear Usuarios'
+        })
+    },
+    listarAgarre: function (req, res) {
+        Usuario.find().exec(function (error, usuariosEncontrados) {
+            if (error) return res.view('error', {
+                title: 'Error',
+                error: {
+                    descripcion: 'Error al listar usuario: ' + error,
+                    url: '/Usuario/listarUsuario'
+                }
+            });
+            return res.view('Usuario/listarUsuario', {
+                title: 'Lista de Usuarios',
+                usuarios: usuariosEncontrados
+            });
+        });
+
+
+    },
+    editarAgarre: function (req, res) {
+        var parametros = req.allParams();
+        if (parametros.id) {
+            Usuario.findOne({
+                id: parametros.id
+            }).exec(function (error, usuarioEncontrado) {
+                if (error) return res.view('error', {
+                    title: 'Error',
+                    error: {
+                        descripcion: 'Error al editar usuario: ' + error,
+                        url: '/Usuario/editarUsuario'
+                    }
+                });
+                return res.view('Usuario/editarUsuario', {
+                    title: 'Editar Usuario -' + usuarioEncontrado.nombre,
+                    usuarios: usuarioEncontrado
+                })
+            })
+        } else {
+            res.view('error', {
+                title: 'Error',
+                error: {
+                    descripcion: 'Error con el ID',
+                    url: '/Usuario/editarUsuario'
+                }
+            });
+        }
+    },
+    error: function (req, res) {
+        return res.view('error', {
+            title: 'Error',
+            error: {
+                descripcion: 'Error en el sistema',
+                url: '/error'
+            }
+        });
+    }
+
+
+};
