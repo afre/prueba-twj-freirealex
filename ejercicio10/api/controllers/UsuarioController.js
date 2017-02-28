e041fb942f636c71a7cbb79cb848194f997e6e42
afre <a.frex_b@hotmail.com>
2017-02-28 11:57:12 -0500

    Parte1

--- ejercicio10/api/controllers/UsuarioController.js
+++ ejercicio10/api/controllers/UsuarioController.js
@@ -6,6 +6,50 @@
  */
 
 module.exports = {
-	
+	crearUsuario:function(req,res){
+        var parametros=req.allParams();
+        if(req.method=='POST'){
+            Usuario.create(
+            {
+                nombre:parametros.nombre,
+                preferencia:parametros.preferencia,
+                fechaNacimiento:parametros.fechaNacimiento
+            }
+            )
+            .exec(
+            function(error,UsuarioCreado){
+                if(error) return res.view('error',{
+                title:'Error',
+                error:{
+                    descripcion:'Error al crear usuario: '+error,
+                    url:'/crearUsuario'
+                }
+            });
+                res.view('/Usuario/listarUsuario/',{
+                    title:'Lista de Usuarios',
+                    usuarios: UsuarioCreado
+                });
+                
+            }
+            );
+        }else{
+            return res.view('error',{
+                title:'Error',
+                error:{
+                    descripcion:'Error en el método HTTP',
+                    url:'/crearUsuario'
+                }
+            })
+        }
+    },
+    listarUsuario:function(req,res){
+        
+    },
+    editarUsuario:function(req,res){
+        
+    },
+    borrarUsuario:function(req,res){
+        
+    }
 };
 
