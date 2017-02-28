e4c2b3a7a598c7a7d8f6c815be19084f5b97e650
afre <a.frex_b@hotmail.com>
2017-02-28 14:29:21 -0500

    Parte3Rutas

--- ejercicio10/api/controllers/RutasController.js
+++ ejercicio10/api/controllers/RutasController.js
@@ -6,6 +6,65 @@
  */
 
 module.exports = {
-	
+home:function(req,res){
+    return res.view('homepage',{
+        title:'Inicio'
+    });
+},
+    crearUsuario:function(req,res){
+        return res.view('Usuario/crearUsuario',{
+            title:'Crear Usuarios'
+        })
+    },
+    listarUsuario:function(req,res){
+        Usuario.find().exec(function(error,usuariosEncontrados){
+            if(error)return res.view('error',{
+                title:'Error',
+                error:{
+                    descripcion:'Error al listar usuario: '+error,
+                    url:'/listarUsuario'
+                }
+            });
+        return res.view('Usuario/listarUsuario',{
+            title:'Lista de Usuarios',
+            usuarios:usuariosEncontrados
+        });
+        });
+        
+    },
+    editarUsuario:function(req,res){
+        var parametros=req.allParams();
+        if(parametros.id){
+            Usuario.findOne({
+                id:parametros.id
+            }).exec(function(error,usuarioEncontrado){
+                if(error)return res.view('error',{
+                title:'Error',
+                error:{
+                    descripcion:'Error al editar usuario: '+error,
+                    url:'/editarUsuario'
+                }
+            });
+                return res.view('Usuario/editarUsuario',{
+                    title: 'Editar Usuario -'+usuarioEncontrado.nombre,
+                    usuarios: usuarioEncontrado
+                })
+            })
+        }else{
+            res.view('error',{
+                title:'Error',
+                error:{
+                    descripcion:'Error con el ID',
+                    url:'/editarUsuario'
+                }
+            });
+        }
+    },
+    
+    crearAgarre:function(req,res){},
+    listarAgarre:function(req,res){},
+    editarAgarre:function(req,res){},
+    
+    
 };
 
--- ejercicio10/config/routes.js
+++ ejercicio10/config/routes.js
@@ -22,28 +22,34 @@
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
+     * etc. depending on your default view engine) your home page.              *
+     *                                                                          *
+     * (Alternatively, remove this and add an `index.html` file in your         *
+     * `assets` directory)                                                      *
+     *                                                                          *
+     ***************************************************************************/
 
-  '/': {
-    view: 'homepage'
-  }
+    'get /': 'RutasController.home',
+    'get /Inicio': 'RutasController.home',
+    'get /CrearUsuario': 'RutasController.crearUsuario',
+    'get /ListarUsuario': 'RutasController.listarUsuario',
+    'get /EditarUsuario': 'RutasController.editarUsuario',
+    'get /CrearAgarre': 'RutasController.crearAgarre',
+    'get /ListarAgarre': 'RutasController.listarAgarre',
+    'get /EditarAgarre': 'RutasController.editarAgarre',
+    'get /Error': 'RutasController.error'    
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Custom routes here...                                                    *
+     *                                                                          *
+     * If a request to a URL doesn't match any of the custom routes above, it   *
+     * is matched against Sails route blueprints. See `config/blueprints.js`    *
+     * for configuration options and examples.                                  *
+     *                                                                          *
+     ***************************************************************************/
 
 };
